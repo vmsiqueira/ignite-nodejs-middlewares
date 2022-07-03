31859b2a6b76db480f68fdc8e09ea952069e624a
Vitor Siqueira <vitormsiqueira@hotmail.com>
2022-07-03 10:31:13 -0300

    desafio completo

--- src/index.js
+++ src/index.js
@@ -10,19 +10,72 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({ error: "User not found" })
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  if(user.pro === false && user.todos.length <= 9) {
+    request.user = user;
+    return next();
+  } else if (user.pro === true) {
+    request.user = user;
+    return next();
+  } else {
+    return response.status(403).json({ error: "User not pro" })
+  }
+
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404).json({ error: 'User does not exist! '});
+  }
+
+  if(!validate(id)){
+    return response.status(400).json({ error: 'Todo id not a UUID! '});
+  }  
+
+  const todo = user.todos.find(todo => todo.id = id);
+
+  if(!todo){
+    return response.status(404).json({ error: 'Todo not found! '});
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user) {
+    return response.status(404).json({ error: "User not found" })
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
